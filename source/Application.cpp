54748b1adabdb0ac05cf5659586c6a9532399a20
Remi Adriaensen <46381050+SleepiDreamer@users.noreply.github.com>
2026-02-20 01:05:15 +0100

    fix: linker errors in release mode

--- source/Application.cpp
+++ source/Application.cpp
@@ -17,7 +17,7 @@ Application::Application(const bool debugLayer)
 	m_camera->SetDirection(glm::vec3(0.0f, 0.0f, -1.0f));
 	m_camera->m_fov = 60.0f;
 	m_renderer->SetCamera(m_camera);
-	m_renderer->LoadModel("assets/models/Sponza/Sponza.gltf");
+	m_renderer->LoadModel("assets/models/LL94Small.glb");
 
 	auto glfwWindow = m_window->GetGLFWWindow();
 
